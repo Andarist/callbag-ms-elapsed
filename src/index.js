7378c010e2acae9e240dbac571136d3d4ccb1c74
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-10-11 22:19:13 +0200

    Remove unused dep - callbag-share

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import animationFrames from 'callbag-animation-frames'
 import flatten from 'callbag-flatten'
 import lazy from 'callbag-lazy'
 import map from 'callbag-map'
-import share from 'callbag-share'
 
 export default flatten(
   lazy(() => {
